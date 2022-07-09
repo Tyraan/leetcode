55c39ab99d7ac8f3c99f688bea7d8ec980669abb
sven <jsdjsven@itcom888.com>
2022-07-09 16:45:29 +0800

    frequent_k problem

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,4 +1,6 @@
 mod problems;
+use std::collections::BinaryHeap;
+
 use problems::solution::Solution;
 
 fn main() {
@@ -7,7 +9,14 @@ fn main() {
     // print!("max area of is {}", res);
     // assert_eq!(res, 49);
 
-    let v = vec![1,2,3,4,5,6,6];
+    let v = vec![1,1,1,15,5,6,6,6];
     let freq =Solution::top_k_frequent(v, 2);
-    assert_eq!(freq, []);
+    assert_eq!(freq, vec![6,5]);
+
+    let mut hv = BinaryHeap::new();
+    for i in vec![1,3,4,5,6,7,8,956,343,4,5,] {
+        hv.push(i);
+    }
+    let q = hv.into_vec();
+    println!("{:?}", q);
 }
--- rust/src/problems/top_k_frequent.rs
+++ rust/src/problems/top_k_frequent.rs
@@ -1,5 +1,4 @@
-use std::collections::HashMap;
-
+use std::{collections::{HashMap, BinaryHeap}, cmp::Reverse};
 use super::solution::Solution;
 
 
@@ -7,7 +6,26 @@ impl Solution {
     pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
         let mut counter = HashMap::new();
         for num in nums {
-            counter.(k, v)
+            counter.entry(num).and_modify(|x| *x += 1).or_insert(1);
         }
+        // 创建一个只保存k个元素的堆，
+        let mut heap =BinaryHeap::new();
+        for (key,freq) in counter {
+            
+            if heap.len() < k as usize  {
+                heap.push(Reverse((freq, key)));
+            } else {
+                let cnt  = heap.peek().unwrap().0 .0;
+                if freq > cnt {
+                    heap.pop();
+                    heap.push(Reverse((freq, key)));
+                }
+            }
+        }
+        let mut res = vec![];
+        while !heap.is_empty() {
+            res.insert(0, heap.pop().unwrap().0.1)
+        }
+        res
     }
 }
